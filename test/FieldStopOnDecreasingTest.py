9157ab5caf624130884a419ba7d137ddeacedbc4
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2019-06-25 10:52:35 +0200

    mu shall be in Field format.

--- test/FieldStopOnDecreasingTest.py
+++ test/FieldStopOnDecreasingTest.py
@@ -66,7 +66,7 @@ class FieldStopOnDecreasingTest(unittest.TestCase):
                                                      PositionAngle.MEAN,
                                                      FramesFactory.getEME2000(),
                                                      date,
-                                                     Constants.EIGEN5C_EARTH_MU))
+                                                     zero.add(Constants.EIGEN5C_EARTH_MU)))
 
         handler = FieldStopOnDecreasing()
         handler_casted = FieldEventHandler.cast_(handler)
@@ -86,7 +86,7 @@ class FieldStopOnDecreasingTest(unittest.TestCase):
                                                      PositionAngle.MEAN,
                                                      FramesFactory.getEME2000(),
                                                      date,
-                                                     Constants.EIGEN5C_EARTH_MU))
+                                                     zero.add(Constants.EIGEN5C_EARTH_MU)))
 
         handler = FieldStopOnDecreasing()
         handler_casted = FieldEventHandler.cast_(handler)
