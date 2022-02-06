5f538127f047f39ddab4ff25625675d62a9c7c39
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2022-02-06 11:24:53 +0100

    cleaning up

--- output/org-stubs/hipparchus/optim/nonlinear/vector/__init__.pyi
+++
@@ -1,8 +0,0 @@
-import org.hipparchus.optim.nonlinear.vector.leastsquares
-import typing
-
-
-class __module_protocol__(typing.Protocol):
-    # A module protocol which reflects the result of ``jp.JPackage("org.hipparchus.optim.nonlinear.vector")``.
-
-    leastsquares: org.hipparchus.optim.nonlinear.vector.leastsquares.__module_protocol__
