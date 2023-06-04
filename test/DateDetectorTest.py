a4a7e2531410d65a19d4b5845d2a89a76c936f91
Petrus Hyvönen <petrus.hyvonen@sscspace.com>
2023-06-04 13:14:27 +0200

    Additional tests
    Additional tests

---
+++ test/DateDetectorTest.py
@@ -0,0 +1,103 @@
+import unittest
+
+import orekit
+orekit.initVM()
+
+from orekit.pyhelpers import  setup_orekit_curdir
+setup_orekit_curdir("resources")
+
+from org.hipparchus.ode.nonstiff import AdaptiveStepsizeIntegrator
+
+from org.orekit.frames import FramesFactory
+from org.orekit.orbits import EquinoctialOrbit, OrbitType
+from org.orekit.propagation import SpacecraftState
+from org.orekit.propagation.events import DateDetector
+from org.orekit.propagation.integration import PythonAdditionalDerivativesProvider, CombinedDerivatives
+from org.orekit.propagation.sampling import PythonOrekitStepHandler
+from org.orekit.propagation.numerical import NumericalPropagator
+from org.orekit.time import AbsoluteDate, TimeScalesFactory
+from org.orekit.utils import PVCoordinates
+from org.hipparchus.geometry.euclidean.threed import Vector3D
+from org.hipparchus.ode.nonstiff import DormandPrince853Integrator
+from orekit import JArray_double
+from orekit.pyhelpers import setup_orekit_curdir
+
+setup_orekit_curdir()
+
+
+class DateDetectorTest(unittest.TestCase):
+
+    def test_simple_timer(self):
+        self.dateDetector = DateDetector(self.maxCheck, self.threshold, self.iniDate.shiftedBy(2.0 * self.dt))
+        self.assertEqual(2 * self.dt, self.dateDetector.getDate().durationFrom(self.iniDate), 1.0e-10)
+
+        class DummyAdditionalDerivativesProvider(PythonAdditionalDerivativesProvider):
+
+            def init(self, initialState, target):
+                pass
+
+            def getName(self):
+                return "dummy"
+
+            def getDimension(self):
+                return 1
+
+            def derivatives(self, state):
+                return JArray_double([0.0])
+
+            def combinedDerivatives(self, s):
+                return CombinedDerivatives(JArray_double([0.0]), None)
+
+            def yield_(self, state):
+                return True
+
+        class InterpolatorStepHandler(PythonOrekitStepHandler):
+
+            def handleStep(self, interpolator, isLast):
+                prev = interpolator.getPreviousState()
+                curr = interpolator.getCurrentState()
+                dt = curr.getDate().durationFrom(prev.getDate())
+                restricted = interpolator.restrictStep(prev.shiftedBy(dt * +0.25), curr.shiftedBy(dt * -0.25))
+                restricted_prev = restricted.getPreviousState()
+                restricted_curr = restricted.getCurrentState()
+                restricted_dt = restricted_curr.getDate().durationFrom(restricted_prev.getDate())
+                self.assertEqual(dt * 0.5, restricted_dt, 1.0e-10)
+
+        self.propagator.addAdditionalDerivativesProvider(DummyAdditionalDerivativesProvider())
+        self.propagator.setInitialState(self.propagator.getInitialState().addAdditionalState("dummy", JArray_double([0.0])))
+
+        self.propagator.getMultiplexer().add(InterpolatorStepHandler())
+        self.propagator.setOrbitType(OrbitType.EQUINOCTIAL)
+        self.propagator.addEventDetector(self.dateDetector)
+        final_state = self.propagator.propagate(self.iniDate.shiftedBy(100.0 * self.dt))
+
+        self.assertEqual(2.0 * self.dt, final_state.getDate().durationFrom(self.iniDate), self.threshold)
+
+
+    def setUp(self):
+        try:
+            mu = 3.9860047e14
+            position = Vector3D(-6142438.668, 3492467.560, -25767.25680)
+            velocity = Vector3D(505.8479685, 942.7809215, 7435.922231)
+            self.iniDate = AbsoluteDate(1969, 7, 28, 4, 0, 0.0, TimeScalesFactory.getTT())
+            self.iniOrbit = EquinoctialOrbit(PVCoordinates(position, velocity), FramesFactory.getEME2000(),
+                                             self.iniDate, mu)
+            initial_state = SpacecraftState(self.iniOrbit)
+            abs_tolerance = [0.001, 1.0e-9, 1.0e-9, 1.0e-6, 1.0e-6, 1.0e-6, 0.001]
+            rel_tolerance = [1.0e-7, 1.0e-4, 1.0e-4, 1.0e-7, 1.0e-7, 1.0e-7, 1.0e-7]
+            integrator = DormandPrince853Integrator(0.001, 1000.0,
+                                                    JArray_double(abs_tolerance),
+                                                    JArray_double(rel_tolerance))
+            integrator.setInitialStepSize(60.0)
+            self.propagator = NumericalPropagator(integrator)
+            self.propagator.setInitialState(initial_state)
+            self.dt = 60.0
+            self.maxCheck = 10.0
+            self.threshold = 10.0e-10
+            self.evtno = 0
+        except Exception as e:
+            self.fail(str(e))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/FixedRateTest.py
@@ -0,0 +1,125 @@
+import unittest
+
+import orekit
+orekit.initVM()
+
+from org.hipparchus.geometry.euclidean.threed import Rotation, Vector3D
+from org.orekit.attitudes import Attitude, FixedRate
+from org.orekit.frames import FramesFactory
+from org.orekit.orbits import KeplerianOrbit
+from org.orekit.time import AbsoluteDate, DateComponents, TimeComponents, TimeScalesFactory
+from org.orekit.utils import PVCoordinates, AngularCoordinates
+from org.hipparchus.util import FastMath
+from org.orekit.orbits import PositionAngle
+from org.orekit.propagation.analytical import KeplerianPropagator
+from org.hipparchus.geometry.euclidean.threed import FieldRotation, FieldVector3D
+from org.hipparchus.util import Decimal64Field
+from org.orekit.orbits import FieldKeplerianOrbit, FieldOrbit
+from org.orekit.propagation import FieldPropagator, FieldSpacecraftState
+from org.orekit.utils import FieldAngularCoordinates, FieldPVCoordinates
+
+from java.io import File
+from org.orekit.data import DataContext, DirectoryCrawler
+
+class OrekitFixedRateTest(unittest.TestCase):
+    def setUp(self):
+        DM = DataContext.getDefault().getDataProvidersManager()
+        datafile = File('../tests_in_development/resources')
+        if not datafile.exists():
+            print('File :', datafile.absolutePath, ' not found')
+
+        crawler = DirectoryCrawler(datafile)
+        DM.clearProviders()
+        DM.addProvider(crawler)
+
+    def test_zero_rate(self):
+        date = AbsoluteDate(DateComponents(2004, 3, 2),
+                            TimeComponents(13, 17, 7.865),
+                            TimeScalesFactory.getUTC())
+        frame = FramesFactory.getEME2000()
+        law = FixedRate(Attitude(date, frame,
+                                 Rotation(0.48, 0.64, 0.36, 0.48, False),
+                                 Vector3D.ZERO, Vector3D.ZERO))
+        pv = PVCoordinates(Vector3D(28812595.32012577, 5948437.4640250085, 0.0),
+                           Vector3D(0.0, 0.0, 3680.853673522056))
+        orbit = KeplerianOrbit(pv, frame, date, 3.986004415e14)
+        attitude0 = law.getAttitude(orbit, date, frame).getRotation()
+        self.assertAlmostEqual(0, Rotation.distance(attitude0, law.getReferenceAttitude().getRotation()), delta=1.0e-10)
+        attitude1 = law.getAttitude(orbit.shiftedBy(10.0), date.shiftedBy(10.0), frame).getRotation()
+        self.assertAlmostEqual(0, Rotation.distance(attitude1, law.getReferenceAttitude().getRotation()), delta=1.0e-10)
+        attitude2 = law.getAttitude(orbit.shiftedBy(20.0), date.shiftedBy(20.0), frame).getRotation()
+        self.assertAlmostEqual(0, Rotation.distance(attitude2, law.getReferenceAttitude().getRotation()), delta=1.0e-10)
+
+
+    def test_non_zero_rate(self):
+        date = AbsoluteDate(DateComponents(2004, 3, 2),
+                            TimeComponents(13, 17, 7.865),
+                            TimeScalesFactory.getUTC())
+        rate = 2 * FastMath.PI / (12 * 60)
+        frame = FramesFactory.getEME2000()
+        gcrf = FramesFactory.getGCRF()
+        law = FixedRate(Attitude(date, frame,
+                                 Rotation(0.48, 0.64, 0.36, 0.48, False),
+                                 Vector3D(rate, Vector3D.PLUS_K), Vector3D.ZERO))
+        ref = law.getReferenceAttitude().getRotation().applyTo(gcrf.getTransformTo(frame, date).getRotation())
+        pv = PVCoordinates(Vector3D(28812595.32012577, 5948437.4640250085, 0.0),
+                           Vector3D(0.0, 0.0, 3680.853673522056))
+        orbit = KeplerianOrbit(pv, frame, date, 3.986004415e14)
+        attitude0 = law.getAttitude(orbit, date, gcrf).getRotation()
+        self.assertAlmostEqual(0, Rotation.distance(attitude0, ref), delta=1.0e-10)
+        attitude1 = law.getAttitude(orbit.shiftedBy(10.0), date.shiftedBy(10.0), gcrf).getRotation()
+        self.assertAlmostEqual(10 * rate, Rotation.distance(attitude1, ref), delta=1.0e-10)
+        attitude2 = law.getAttitude(orbit.shiftedBy(-20.0), date.shiftedBy(-20.0), gcrf).getRotation()
+        self.assertAlmostEqual(20 * rate, Rotation.distance(attitude2, ref), delta=1.0e-10)
+        self.assertAlmostEqual(30 * rate, Rotation.distance(attitude2, attitude1), delta=1.0e-10)
+        attitude3 = law.getAttitude(orbit.shiftedBy(0.0), date, frame).getRotation()
+        self.assertAlmostEqual(0, Rotation.distance(attitude3, law.getReferenceAttitude().getRotation()), delta=1.0e-10)
+
+
+    def test_spin(self):
+        date = AbsoluteDate(DateComponents(1970, 1, 1),
+                            TimeComponents(3, 25, 45.6789),
+                            TimeScalesFactory.getUTC())
+
+        rate = 2 * FastMath.PI / (12 * 60)
+        law = FixedRate(Attitude(date, FramesFactory.getEME2000(),
+                                 Rotation(0.48, 0.64, 0.36, 0.48, False),
+                                 Vector3D(rate, Vector3D.PLUS_K),
+                                 Vector3D.ZERO))
+
+        orbit = KeplerianOrbit(7178000.0, 1.e-4, FastMath.toRadians(50.0),
+                               FastMath.toRadians(10.0), FastMath.toRadians(20.0),
+                               FastMath.toRadians(30.0), PositionAngle.MEAN,
+                               FramesFactory.getEME2000(), date, 3.986004415e14)
+
+        propagator = KeplerianPropagator(orbit, law)
+
+        h = 0.01
+        s_minus = propagator.propagate(date.shiftedBy(-h))
+        s0 = propagator.propagate(date)
+        s_plus = propagator.propagate(date.shiftedBy(h))
+
+        error_angle_minus = Rotation.distance(s_minus.shiftedBy(h).getAttitude().getRotation(),
+                                              s0.getAttitude().getRotation())
+        evolution_angle_minus = Rotation.distance(s_minus.getAttitude().getRotation(),
+                                                  s0.getAttitude().getRotation())
+        self.assertAlmostEqual(0.0, error_angle_minus, delta=1.0e-6 * evolution_angle_minus)
+
+        error_angle_plus = Rotation.distance(s0.getAttitude().getRotation(),
+                                             s_plus.shiftedBy(-h).getAttitude().getRotation())
+        evolution_angle_plus = Rotation.distance(s0.getAttitude().getRotation(),
+                                                 s_plus.getAttitude().getRotation())
+        self.assertAlmostEqual(0.0, error_angle_plus, delta=1.0e-6 * evolution_angle_plus)
+
+        spin0 = s0.getAttitude().getSpin()
+        reference = AngularCoordinates.estimateRate(s_minus.getAttitude().getRotation(),
+                                                    s_plus.getAttitude().getRotation(),
+                                                    2 * h)
+        self.assertAlmostEqual(0.0, spin0.subtract(reference).getNorm(), delta=1.0e-14)
+
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(OrekitFixedRateTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
